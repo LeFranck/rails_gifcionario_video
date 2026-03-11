a5b0acb71573c39eb0b7fb9143bd131f2271880d
LeFranck <franco.ac.2020@gmail.com>
2026-03-10 23:18:43 -0300

    photos & home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :posts
+  get "pages/home", to: "pages#home"
+  get "pages/contact", to: "pages#contact"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
